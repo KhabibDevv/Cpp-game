6f57b58ff3f8a4a3ef1e678516c454fbaeedb238
KhabibDevv <134228639+KhabibDevv@users.noreply.github.com>
2024-03-12 23:39:38 +0100

    Update RCCollision() function SFML.cpp

--- RCCollision() function SFML.cpp
+++ RCCollision() function SFML.cpp
@@ -1,4 +1,4 @@
-bool RCCollision(sf::RectangleShape Rectangle, sf::CircleShape Circle)
+bool RCCollision(sf::RectangleShape Rectangle, sf::CircleShape Circle) // function that returns true if a Rectangle and a sphere are coliding, false if not
 {
     if (abs(Rectangle.getPosition().x - Circle.getPosition().x) < abs(Circle.getRadius() + Rectangle.getSize().x / 2) && abs(Rectangle.getPosition().y - Circle.getPosition().y) < abs(Circle.getRadius() + Rectangle.getSize().y / 2)) {
         return true;
